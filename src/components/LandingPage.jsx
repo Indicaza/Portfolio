b07e179c6fe6cab56ba62646b9945ea993f043fa
Indicaza <indicazas@gmail.com>
2023-06-28 00:50:16 -0400

    sizing trouble

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -9,29 +9,23 @@ function LandingPage() {
 			className="pb-30 z-0 h-screen w-screen bg-accent1"
 		>
 			<div className="flex h-full items-center justify-center">
-				<div className="flex h-1/2 w-1/2 items-center justify-center">
-					<div className="">
-						<img
-							className="mx-auto w-1/2 rounded-full"
-							src={hacker}
-							alt="Hackerman"
-						/>
-					</div>
-					<div className="absolute">
-						<img
-							className="animate-spin-slow mx-auto w-1/2"
-							src={oneRing}
-							alt="One Ring to Rule Them All, One Ring to Find Them, One Ring to Bring Them All and in the Darkness Bind Them."
-						/>
-					</div>
-				</div>
+				<div className="flex items-center justify-center">
+					<img
+						className="mx-auto w-1/2 rounded-full"
+						src={hacker}
+						alt="Hackerman"
+					/>
 
-				<div className="absolute mt-60 flex w-full justify-center pt-60">
-					<div className="">
+					<img
+						className="animate-spin-slow absolute mx-auto"
+						src={oneRing}
+						alt="One Ring to Rule Them All, One Ring to Find Them, One Ring to Bring Them All and in the Darkness Bind Them."
+					/>
+
+					<div className="absolute bottom-0 flex w-screen justify-center pb-20">
 						<img
 							alt="GitHub Stats"
 							src="https://github-readme-streak-stats.herokuapp.com/?user=Indicaza&theme=darcula&hide_border=true"
-							className=""
 						/>
 					</div>
 				</div>
@@ -44,7 +38,7 @@ function LandingPage() {
           }
 
           .animate-spin-slow {
-            animation: spin 45s linear infinite;
+            animation: spin 60s linear infinite;
           }
         `}
 			</style>
--- src/components/PortfolioSection.jsx
+++ src/components/PortfolioSection.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 
 function PortfolioSection() {
-  return (
-    <section id="portfolio" className="z-10 relative h-screen w-screen flex justify-center items-center">
-      <div className="flex flex-col justify-center items-center rounded-md shadow-inner">
-
-        <div className="flex items-center justify-center h-screen w-screen mt-0 pt-0">
+	return (
+		<section
+			id="portfolio"
+			className="relative z-10 flex h-screen w-screen items-center justify-center"
+		>
+			<div className="flex flex-col items-center justify-center rounded-md shadow-inner">
+				{/* <div className="flex items-center justify-center h-screen w-screen mt-0 pt-0">
           <img alt="GitHub Stats" src="https://github-readme-streak-stats.herokuapp.com/?user=Indicaza&theme=darcula&hide_border=true" className="w-1/2 h-1/2" />
-        </div>
-      </div>    
-    </section>
-  );
+        </div> */}
+			</div>
+		</section>
+	);
 }
 
 export default PortfolioSection;
